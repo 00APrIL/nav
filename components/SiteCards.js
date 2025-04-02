c6d7e941689a1895d70ea9461956d52278573eab
00APrIL <113167968+00APrIL@users.noreply.github.com>
2025-04-03 00:17:19 +0800

    Create SiteCards.js

---
+++ components/SiteCards.js
@@ -0,0 +1,39 @@
+import { FiEdit2, FiTrash2 } from 'react-icons/fi'
+
+export default function SiteCard({ site, onEdit, onDelete }) {
+  return (
+    <div className="bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow p-4 flex flex-col">
+      <a 
+        href={site.url} 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="flex-1 flex flex-col items-center text-center group"
+      >
+        <div className="text-4xl mb-2">{site.icon}</div>
+        <h3 className="font-medium text-gray-800 group-hover:text-blue-600 transition-colors">
+          {site.name}
+        </h3>
+        <p className="text-xs text-gray-500 truncate w-full mt-1">
+          {site.url.replace(/^https?:\/\//, '').replace(/\/$/, '')}
+        </p>
+      </a>
+      
+      <div className="flex justify-center mt-4 pt-2 border-t border-gray-100">
+        <button
+          onClick={onEdit}
+          className="text-gray-500 hover:text-blue-600 p-1 mx-1"
+          title="编辑"
+        >
+          <FiEdit2 size={16} />
+        </button>
+        <button
+          onClick={onDelete}
+          className="text-gray-500 hover:text-red-600 p-1 mx-1"
+          title="删除"
+        >
+          <FiTrash2 size={16} />
+        </button>
+      </div>
+    </div>
+  )
+}
